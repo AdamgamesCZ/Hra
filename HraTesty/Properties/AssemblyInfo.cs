6aaf98557c6e68addba82f8ad5248d2160d9f095
eliasp.06 <eliasp.06@A222-12>
2024-04-26 10:32:21 +0200

    Přidejte soubory projektu.

---
+++ HraTesty/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HraTesty")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HraTesty")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22c95765-d031-4049-a350-0514299bd5e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HraTesty/UnitTest1.cs
@@ -0,0 +1,124 @@
+﻿using Hra;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HraTesty
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public Hrac hrac = new Hrac("Pepik", 10, 2.24, 2.45);
+        
+        [TestMethod]
+        public void TestDelkyJmena()
+        {
+            Assert.IsTrue(hrac.Jmeno.Length < 10);
+        }
+
+        [TestMethod]
+        public void TestDelkyJmenaZapor()
+        {
+            Assert.IsFalse(hrac.Jmeno.Length < 10);
+        }
+
+        [TestMethod]
+        public void TestPoziceX()
+        {
+            Assert.IsTrue(hrac.poziceX == 0);
+        }
+
+        [TestMethod]
+        public void TestPoziceXZapor()
+        {
+            hrac.poziceX = 10;
+            Assert.IsFalse(hrac.poziceX == 0);
+        }
+
+        [TestMethod]
+        public void TestPoziceY()
+        {
+            Assert.IsTrue(hrac.poziceY == 0);
+        }
+
+        [TestMethod]
+        public void TestPoziceYZapor()
+        {
+            hrac.poziceY = 10;
+            Assert.IsFalse(hrac.poziceY == 0);
+        }
+
+        [TestMethod]
+        public void TestOblicej()
+        {
+            Hrac adamek = new Hrac("adamek", "Kouzelník", 0, 0, 0);
+            Assert.IsTrue(adamek.oblicej == 0);
+        }
+
+        [TestMethod]
+        public void TestOblicejZapor()
+        {
+            Hrac adamek = new Hrac("adamek", "Kouzelník", 3, 0, 0);
+            Assert.IsFalse(adamek.oblicej == 0);
+        }
+
+        [TestMethod]
+        public void TestVlasy()
+        {
+            Hrac adamek = new Hrac("adamek", "Kouzelník", 0, 0, 0);
+            Assert.IsTrue(adamek.vlasy == 0);
+        }
+        [TestMethod]
+        public void TestVlasyZapor()
+        {
+            Hrac adamek = new Hrac("adamek", "Kouzelník", 0, 3, 0);
+            Assert.IsFalse(adamek.vlasy == 0);
+        }
+
+        [TestMethod]
+        public void TestBarvaVlasu()
+        {
+            Hrac adamek = new Hrac("adamek", "Kouzelník", 3, 0, 0);
+            Assert.IsTrue(hrac.barvaVlasu == 0);
+        }
+
+        [TestMethod]
+        public void TestBarvaVlasuZapor()
+        {
+            Hrac adamek = new Hrac("adamek", "Kouzelník", 3, 0, 0);
+            Assert.IsFalse(hrac.barvaVlasu == 0);
+        }
+
+        [TestMethod]
+        public void Specializace()
+        {
+            Hrac adamek = new Hrac("adamek", "Kouzelník", 3, 0, 0);
+            Assert.IsTrue(hrac.Specializace.Contains("Kouzelník"));
+            Assert.IsTrue(hrac.Specializace.Contains("Berseker"));
+            Assert.IsTrue(hrac.Specializace.Contains("Inženýr"));
+            Assert.IsTrue(hrac.Specializace.Contains("Cizák"));
+
+        }
+        [TestMethod]
+        public void TestLevel()
+        {
+            Assert.IsTrue(hrac.level == 1);
+        }
+        [TestMethod]
+        public void TestLevelZapor()
+        {
+            hrac.level = 3;
+            Assert.IsFalse(hrac.level == 1);
+        }
+
+        [TestMethod]
+        public void TestXP()
+        {
+            hrac.level = 3;
+            Hrac adamek = new Hrac("adamek", "Kouzelník", 3, 0, 0);
+            hrac.PridejXP();
+            Assert.IsTrue(alex.XP == 100 * steve.level);
+        }
+        
+
+    }
+}
